d6cf94f3019cba9f7b574d6309d1bfd92596d681
RobinBuschmann <robinbuschmann@hotmail.de>
2017-10-30 15:36:43 +0100

    basic layout setup with antd

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
-import {Button} from 'antd';
+import {Layout} from 'antd';
 import './App.scss';
 
 export const App = () => (
-  <div className="app">Hello World
-    <Button onClick={() => console.log("test")}>Click me</Button>
-  </div>
+  <Layout className={'app'}>
+    <Layout.Sider collapsed={true}>Nav</Layout.Sider>
+    <Layout.Content className={'left-container'} style={{background: '#fff'}}>
+      Image
+    </Layout.Content>
+    <Layout.Content className={'main-container'} style={{background: '#ff95ab'}}>
+      Content
+    </Layout.Content>
+  </Layout>
 );
